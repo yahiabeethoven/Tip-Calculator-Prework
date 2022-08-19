152dfb27fea7616121a164376654bc7535a3d477
yahiabeethoven <ybt214@nyu.edu>
2022-08-19 17:13:58 +0200

    Initial Commit

---
+++ Tip Calculator Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator Prework
+//
+//  Created by Yahia Beethoven on 19/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tip Calculator PreworkUITests/Tip_Calculator_PreworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tip_Calculator_PreworkUITestsLaunchTests.swift
+//  Tip Calculator PreworkUITests
+//
+//  Created by Yahia Beethoven on 19/8/22.
+//
+
+import XCTest
+
+class Tip_Calculator_PreworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
